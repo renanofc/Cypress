c023890a6d8926166937d88b8c2f8324ed406258
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-16 18:50:57 -0300

    Update

--- cypress/integration/3-alurapic/alurapic.spec.js
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -31,12 +31,12 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
     })
 
-    it.only('fazer login de usuario valido', () => {
+    it('fazer login de usuario valido', () => {
         cy.login('flavio', '123')
         cy.contains('a', '(Logout)').should('be.vesible');
     })
 
-    it.only('fazer login de usuario invalido', () => {
+    it('fazer login de usuario invalido', () => {
         cy.login('aaaaass', '2121sss')
         cy.on ('window:alert', (str) => {
             expect(str).to.equal('Invalid user name or password')
