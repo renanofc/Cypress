8501ede18fd93ad94d74aa01523c6b2f52dd1aa6
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-19 15:48:13 -0300

    Update

--- cypress/integration/3-alurapic/alurapic.spec.js
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -43,19 +43,19 @@ describe('Login e registro de usuarios alura pic', () => {
         })
     })
     
-    const usuarios = require('../../fixtures/usuarios.json');
-    usuarios.forEach(usuario => {
+//    const usuarios = require('../../fixtures/usuarios.json');
+//    usuarios.forEach(usuario => {
 
-        it.only('registra novo usuario' + usuario.userName, () => {
-            cy.contains('a', 'Register now').click();
-            cy.contains('button', 'Register').click();
-            cy.get('input[formcontrolname="email"]').type(usuario.email);
-            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
-            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
-            cy.get('input[formcontrolname="password"]').type(usuario.password);
-            cy.contains('button', 'Register').click();
-        }
+//       it.only('registra novo usuario' + usuario.userName, () => {
+//            cy.contains('a', 'Register now').click();
+//            cy.contains('button', 'Register').click();
+//            cy.get('input[formcontrolname="email"]').type(usuario.email);
+//            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+//            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+//            cy.get('input[formcontrolname="password"]').type(usuario.password);
+//           cy.contains('button', 'Register').click();
+//        }
 
-    
+   // })
     
 })
