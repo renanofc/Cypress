ecf91238d447c749795fd4ccfd70a326c5a94010
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-19 15:24:53 -0300

    Update

--- cypress/integration/3-alurapic/alurapic.spec.js
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -41,5 +41,21 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.on ('window:alert', (str) => {
             expect(str).to.equal('Invalid user name or password')
         })
-    })   
+    })
+    
+    const usuarios = require('../../fixtures/usuarios.json');
+    usuarios.forEach(usuario => {
+
+        it.only('registra novo usuario' + usuario.userName, () => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register').click();
+        }
+
+    
+    
 })
